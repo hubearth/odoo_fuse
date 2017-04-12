6b2b16370877faaa1b8e6ab9f234d008299f833d
hubearth <tech@sesum.org>
2017-04-11 23:38:48 -0400

    .

--- static/src/js/fuse_calendar_customization.js
+++ static/src/js/fuse_calendar_customization.js
@@ -4,16 +4,34 @@ odoo.define('fuse_calendar_customization.fuse_calendar_customization', function 
 var widgets = require('calendar.widgets');
 
 // Extend widget
-/*widgets.SidebarFilter.include({
-	init: function () {
-		this.set_ViewEveryBody_true();
-	},
+widgets.SidebarFilter.include({
+	load_favorite_list: function () {
+        var self = this;
+        // Untick sidebar's filters if there is an active partner in the context
+        var active_partner = (this.view.dataset.context.active_model === 'res.partner');
+        return session.is_bound.then(function() {
+            self.view.all_filters = {};
+            self.view.now_filter_ids = [];
+            self._add_filter(session.partner_id, session.name + _lt(" [Me]"), !active_partner);
+            self._add_filter(-1, _lt("Everybody's calendars"), true, false);
+            //Get my coworkers/contacts
+            return new Model("calendar.contacts")
+                .query(["partner_id"])
+                .filter([["user_id", "=", session.uid]])
+                .all()
+                .then(function(result) {
+                    _.each(result, function(item) {
+                        self._add_filter(item.partner_id[0], item.partner_id[1], !active_partner, true);
+                    });
 
-	set_ViewEveryBody_true: function () {
-		self._add_filter(-1, _lt("Everybody's calendars"), true, false);
+                    self.view.now_filter_ids = _.pluck(self.view.all_filters, 'value');
+
+                    self.render();
+                });
+        });
 	},
-});*/
-widgets.SidebarFilter._add_filter(-1, _lt("Everybody's calendars"), true, false);
+});
+//widgets.SidebarFilter._add_filter(-1, _lt("Everybody's calendars"), true, false);
 	
 
 });
