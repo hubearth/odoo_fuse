0764d7c37d77c3da05c4ed7db6c89fe55f9c9041
hubearth <tech@sesum.org>
2017-04-16 21:23:12 -0400

    .

--- static/src/js/fuse_calendar_customization.js
+++ static/src/js/fuse_calendar_customization.js
@@ -9,7 +9,7 @@ odoo.define('fuse_calendar_customization.everybodys_calendar', function (require
 	//calendar.widgets.SidebarFilter._add_filter(654, _lt("crotte de boeuf"), true, false);
 	
 	// Extend widget
-	var everybodys_calendar = widgets.SidebarFilter.extend({
+	/*var everybodys_calendar = widgets.SidebarFilter.extend({
 		load_favorite_list: function () {
 			this._super();
 			return session.is_bound.then(function() {
@@ -17,14 +17,9 @@ odoo.define('fuse_calendar_customization.everybodys_calendar', function (require
 			});
 			
 		},
-		/*
-		set_default_everybodyscalendar: function (){
-			this._add_filter(654, _lt("crotte de boeuf"), true, false);
-			
-		},
-	*/
+		
 
-	});
+	});*/
 
 		/*
 		load_favorite_list: function () {
