8f41e13c5c90633b138b778dcc86e5b8084498cd
hubearth <tech@sesum.org>
2017-04-19 00:24:25 -0400

    .

--- static/src/js/fuse_calendar_customization.js
+++ static/src/js/fuse_calendar_customization.js
@@ -12,7 +12,7 @@ odoo.define('fuse_calendar_customization.everybodys_calendar', function (require
 	//widgets.SidebarFilter._add_filter(-1, _lt("Everybody's calendars"), false, false);
 	//calendar.widgets.SidebarFilter._add_filter(654, _lt("crotte de boeuf"), true, false);
 	
-	console.log("fuse_calendar loaded:");
+	console.log("fuse_calendar loaded: " + widgets.SidebarFilter.extend);
 	
 	for(var propName in widgets.SidebarFilter.extend) {
 	    
