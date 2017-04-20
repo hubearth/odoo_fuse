f2ff701d43e56126581574de9c6ad1fd59d08245
hubearth <tech@sesum.org>
2017-04-20 00:00:34 -0400

    .

--- static/src/js/fuse_calendar_customization.js
+++ static/src/js/fuse_calendar_customization.js
@@ -1,7 +1,7 @@
 //odoo.define('fuse_calendar_customization.everybodys_calendar', function (require){
 //	"use strict";
 
-odoo.fuse_calendar_customization = function(instance, local) {
+odoo.odoo_fuse = function(instance, local) {
 	// require original module JS
 	//var widgets = require('base_calendar.widgets');
 	var calendar = require('base_calendar.base_calendar');
