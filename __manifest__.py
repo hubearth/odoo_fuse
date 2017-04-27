7e3d737252a385010a013e74a46b399bd6df8126
hubearth <tech@sesum.org>
2017-04-26 22:55:33 -0400

    project initial commit

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,8 @@
      'security/disableling_security.xml',
      'security/ir.model.access.csv',
      'views/fuse_disableling.xml',
-     'views/fuse_calendar_customization_view.xml'    
+     'views/fuse_calendar_customization_view.xml',
+     'views/fuse_project.xml'    
  ], 
  'test': [ 
      'static/tests/test_fuse_calendar_customization.js'     
